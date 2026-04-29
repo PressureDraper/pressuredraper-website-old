e5dbee06b3a7df4d7160fd0371aff2b653aaacd2
PressureDraper <warmarines00@hotmail.com>
2026-04-29 13:44:22 -0600

    fix(files): vite.config base route

--- vite.config.ts
+++ vite.config.ts
@@ -4,46 +4,46 @@ import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 100,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
-    }),
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  },
-  build: {
-    outDir: 'docs',
-  },
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-  },
-  /* base: "/pressuredraper-website" */ //route base for github pages without domain
-  base: "/"
+    plugins: [
+        react(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 100,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false,
+                    },
+                ],
+            },
+        }),
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    },
+    build: {
+        outDir: 'docs',
+    },
+    define: {
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    },
+    base: "/pressuredraper-website-old" //route base for github pages without domain
+    /* base: "/" */
 })
